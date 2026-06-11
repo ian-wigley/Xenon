479fd550a2af852d1c1753419650f24b68d08eec
Ian Wigley <ian.wigley@outlook.com>
2026-06-11 18:43:04 +0100

    Update Particle.ts

--- Xenon_VSCode/Particle.ts
+++ Xenon_VSCode/Particle.ts
@@ -3,9 +3,9 @@
 class Particle {
 
     m_position: gsCVector;
-    m_velocity:gsCVector;
-    m_mass:number;
-    m_age:number;
+    m_velocity: gsCVector;
+    m_mass: number;
+    m_age: number;
     m_lifetime: number;
     m_name = "Particle";
 }
