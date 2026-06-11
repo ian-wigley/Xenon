734b347373573d3665bd3737c0cf8ac9904b99d8
Ian Wigley <ian.wigley@outlook.com>
2026-06-11 18:39:46 +0100

    Update TiledImage.ts

--- Xenon_VSCode/TiledImage.ts
+++ Xenon_VSCode/TiledImage.ts
@@ -1,8 +1,6 @@
 ﻿import gsCImage = require("Image");
-import gsCPoint = require("Point");
 import gsCScreen = require("Screen");
 import gsCRectangle = require("Rectangle");
-import gsCVector = require("Vector");
 import Point = require("Point");
 
 class gsCTiledImage extends gsCImage {
@@ -15,7 +13,7 @@ class gsCTiledImage extends gsCImage {
     constructor(imageTile: HTMLImageElement) {
         super();
         this.m_imageTiles = imageTile;
-        var m_rect: gsCRectangle = new gsCRectangle(0, 0, imageTile.width, imageTile.height);
+        const m_rect: gsCRectangle = new gsCRectangle(0, 0, imageTile.width, imageTile.height);
         this.setSize(m_rect);
         this.m_screen = new gsCScreen;
     }
@@ -35,13 +33,13 @@ class gsCTiledImage extends gsCImage {
             return false;
         }
 
-        var horiz: number = this.getSize().x / this.m_tile_size.X;
-        var vert: number = this.getSize().y / this.m_tile_size.Y;
+        const horiz: number = this.getSize().x / this.m_tile_size.X;
+        const vert: number = this.getSize().y / this.m_tile_size.Y;
         this.m_num_tiles = horiz * vert;
         this.m_source_rects = [];
 
-        for (var y = 0; y + this.m_tile_size.Y <= this.getSize().y; y += this.m_tile_size.Y) {
-            for (var x = 0; x + this.m_tile_size.X <= this.getSize().x; x += this.m_tile_size.X) {
+        for (let y = 0; y + this.m_tile_size.Y <= this.getSize().y; y += this.m_tile_size.Y) {
+            for (let x = 0; x + this.m_tile_size.X <= this.getSize().x; x += this.m_tile_size.X) {
                 this.m_source_rects.push(new gsCRectangle(x, y, x + this.m_tile_size.X, y + this.m_tile_size.Y));
             }
         }
@@ -73,19 +71,18 @@ class gsCTiledImage extends gsCImage {
     // Main tile Drawing method !! <gsCTiledImage>
     public draw(tile: number, position: Point, ctx: CanvasRenderingContext2D): boolean {
 
-        if (tile != 6.208 && tile != 0.208 && tile != 19.200000000000003 && tile != 20.208  ) {
+        if (tile != 6.208 && tile != 0.208 && tile != 19.200000000000003 && tile != 20.208) {
 
             if (tile >= this.m_source_rects.length) {
                 console.log("Tile number greater than the number of tiles - Draw()");
                 return false;
             }
 
-            var dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
+            const dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
             if (this.m_screen.contains(dest)) {
                 ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
                 return true;
-            }
-            else {
+            } else {
                 ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
                 //return true;
                 return false;
@@ -102,12 +99,11 @@ class gsCTiledImage extends gsCImage {
             return false;
         }
         //Add fill colour
-        var dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
+        const dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
         if (this.m_screen.contains(dest)) {
             ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
             return true;
-        }
-        else {
+        } else {
             ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
             return true;
         }
@@ -121,11 +117,10 @@ class gsCTiledImage extends gsCImage {
             return false;
         }
         //Add tint colour
-        var dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
+        const dest: gsCRectangle = new gsCRectangle(position.X, position.Y, position.X + this.m_tile_size.X, position.Y + this.m_tile_size.Y);
         if (this.m_screen.contains(dest)) {
             ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
-        }
-        else {
+        } else {
             ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
         }
         return true;
@@ -134,7 +129,7 @@ class gsCTiledImage extends gsCImage {
     //-------------------------------------------------------------
 
     public drawFast(tile: number, position: Point, ctx: CanvasRenderingContext2D): boolean {
-        var source: gsCRectangle = this.m_source_rects[tile];
+        const source: gsCRectangle = this.m_source_rects[tile];
         ctx.drawImage(this.m_imageTiles, this.m_source_rects[tile].Left, this.m_source_rects[tile].Top, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height, position.X, position.Y, this.m_source_rects[tile].Width, this.m_source_rects[tile].Height);
         return true;
     }
