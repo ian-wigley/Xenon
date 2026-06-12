700d745fe1b61167ab95d8b5244768a4376894bd
Ian Wigley <ian.wigley@outlook.com>
2026-06-12 12:00:34 +0100

    Update DustEffect.ts

--- Xenon_VSCode/DustEffect.ts
+++ Xenon_VSCode/DustEffect.ts
@@ -16,13 +16,13 @@ class CDustEffect extends CParticleEffect {
         if (this.m_timer.getTime() > 0.05) {
             this.m_timer.start();
 
-            var p: Particle = new Particle();
-
+            const p: Particle = new Particle();
             p.m_position = this.getPosition();
+
             //this.p.m_velocity = new gsCVector::polar(1.0, m_random.getFloat(360.0)) + gsCVector(0.0, 1.0);
             //p.m_velocity = new gsCVector(1.0, Math.floor(Math.random() * 360.0)).plus1(new gsCVector(0.0, 1.0));
 
-            var temp: gsCVector = new gsCVector(0, 0);
+            const temp: gsCVector = new gsCVector(0, 0);
             p.m_velocity = temp.polar(1.0, (Math.random() * 360.0) + 1.0);
 
             p.m_age = 0.0;
@@ -30,8 +30,7 @@ class CDustEffect extends CParticleEffect {
             p.m_mass = 1.0;
 
             return p;
-        }
-        else {
+        } else {
             return null;
         }
     }
